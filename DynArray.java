fae2ad5bb33d0ef6e93426ddecfce5924e44d02e
HBodenstein <92017225+MrGroundstone@users.noreply.github.com>
2025-06-03 07:23:35 +0200

    Update DynArray.java

--- DynArray.java
+++ DynArray.java
@@ -142,11 +142,19 @@ public class DynArray<T> {
         }
     }
 
+    /**
+     * Gibt den Inhalt der Liste in eckigen Klammern auf der Konsole aus.
+     * Jedes Element wird durch ein Komma getrennt. Wenn ein Element eine Instanz
+     * von {@code BinTree} ist, wird der Text des Items des Baumes ausgegeben.
+     * Andernfalls wird das Element direkt ausgegeben.
+     * 
+     * Beispielausgabe: [A,B,C,]
+     */
     public void print() {
         PApplet.print("[");
         for (int i = 0; i < laenge; i++) {
             if (this.getItem(i) instanceof BinTree b) {
-                PApplet.print( b.getItem().getText() + ",");
+                PApplet.print(b.getItem().getText() + ",");
             }else{
                 PApplet.print(this.getItem(i) + ",");
             }
